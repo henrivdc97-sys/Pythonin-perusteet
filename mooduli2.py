614d020dfca2d236d0237bd19d8ead5e7cc3a9d4
henrivan <henri.vdc97@gmail.com>
2026-02-11 10:33:54 +0200

    Moduuli 2 tehtävät 1-6

--- mooduli2.py
+++ mooduli2.py
@@ -1,13 +1,17 @@
 
+
+#tehtävä 1
 nimi = input('Anna nimesi:')
 print(f'Terve, {nimi}!')
 
+#tehtävä 2
 import math
 
 sade = float(input("Anna ympyrän säde: "))
 pinta_ala = math.pi * sade ** 2
 print(pinta_ala)
 
+#tehtävä 3
 kanta = float(input('Anna suorakulmion kanta'))
 korkeus = float(input('Anna suorakulmion korkeus'))
 
@@ -17,6 +21,8 @@ Piiri = 2 * kanta + 2 * korkeus
 print(pinta_ala)
 print(Piiri)
 
+
+#tehtävä 4
 luku1, luku2, luku3 = map(int, input('Anna kolme lukua').split())
 
 summa = luku1 + luku2 + luku3
@@ -27,11 +33,12 @@ print(summa)
 print(tulo)
 print(keskiarvo)
 
+#tehtävä 5
 leiviskat = float(input("Anna leiviskät"))
 naulat = float(input("Anna naulat"))
 luodit = float(input("Anna luodit"))
 
-# Muunnokset grammoiksi
+
 grammat = (
     leiviskat * 20 * 32 * 13.3 +
     naulat * 32 * 13.3 +
@@ -44,5 +51,19 @@ jaannos_grammat = grammat % 1000
 print("Massa nykymittojen mukaan:")
 print(f"{kilogrammat} kilogrammaa ja {jaannos_grammat} grammaa.")
 
+#tehtävä 6
+import random
 
+
+koodi3 = ""
+for i in range(3):
+    koodi3 += str(random.randint(0, 9))
+
+
+koodi4 = ""
+for i in range(4):
+    koodi4 += str(random.randint(1, 6))
+
+print("Kolmenumeroinen koodi (0–9):", koodi3)
+print("Nelinumeroinen koodi (1–6):", koodi4)
 
