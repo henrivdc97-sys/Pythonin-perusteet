76494a63bae907049ddc19c7875e430cee4ed691
henrivan <henri.vdc97@gmail.com>
2026-02-11 13:53:18 +0200

    Comittasin moodulit uudestaan erikseen

---
+++ moduuli 2/Keskiarvo.py
@@ -0,0 +1,10 @@
+#tehtävä 4
+luku1, luku2, luku3 = map(int, input('Anna kolme lukua').split())
+
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = (luku1 + luku2 + luku3) / 3
+
+print(summa)
+print(tulo)
+print(keskiarvo)
